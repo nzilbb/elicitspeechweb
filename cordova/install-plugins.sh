d3c815966af6d275f35e0a0c4a113850116db8e5
Robert Fromont <robert.fromont@canterbury.ac.nz>
2017-01-12 13:42:59 -0300

    Install plugins script.

---
+++ cordova/install-plugins.sh
@@ -0,0 +1,5 @@
+cordova plugin add cordova-plugin-audioinput
+cordova plugin add cordova-plugin-compat
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-file
+cordova plugin add cordova-plugin-app-version
