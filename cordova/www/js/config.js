d4a8e917b8d9fb898c75cd9e3a08cb1030180de8
Robert Fromont <robert@fromont.net.nz>
2017-01-25 13:04:20 -0300

    Default schedule times to work with values accepted by input of type "time"

--- cordova/www/js/config.js
+++ cordova/www/js/config.js
@@ -2,7 +2,7 @@
 var config = {
     url : "http://192.168.1.145:8080/labbcat/elicit/steps",
     tasks : {
-	"speech" : ["8:00", "14:00", "20:00"],
+	"speech" : ["08:00", "14:00", "20:00"],
 	"diary" : ["22:00"]
     }
 };
