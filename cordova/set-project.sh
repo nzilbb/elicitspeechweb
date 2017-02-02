48f377c5726671e22f769ca75347e9df7cf904ae
Robert Fromont <robert@fromont.net.nz>
2017-02-02 14:20:19 -0300

    Add project management, so different apps can be built from the same codebase.

---
+++ cordova/set-project.sh
@@ -0,0 +1,8 @@
+if [ -d "configurations/$1" ]; then
+    cp projects/$1/config.xml .
+    cp projects/$1/config.js www/js/
+    cp projects/$1/icon.png www/img/
+    echo Configuration files for $1 copied
+else
+    echo $1 is not a valid project
+fi
