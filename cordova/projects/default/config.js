10e31bb8f208505ad3c6eb798d0f6f3744e6d8b7
Robert Fromont <robert@fromont.net.nz>
2017-02-19 19:37:10 -0300

    Update local IP address.

--- cordova/projects/default/config.js
+++ cordova/projects/default/config.js
@@ -1,5 +1,5 @@
 // app-specific configuration
 var config = {
-    url : "http://192.168.1.45:8080/labbcat/elicit/steps",
+    url : "http://192.168.1.145:8080/labbcat/elicit/steps",
     tasks : [ "speech" ]
 };
