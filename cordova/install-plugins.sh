feda4dcbdadaf32d18f87fb4f04908394baef65d
Robert Fromont <robert.fromont@canterbury.ac.nz>
2017-07-11 12:57:44 -0300

    Use modified version of cordova-plugin-audioinput.

--- cordova/install-plugins.sh
+++ cordova/install-plugins.sh
@@ -1,9 +1,13 @@
-cordova plugin add cordova-plugin-audioinput
+#cordova plugin add cordova-plugin-audioinput
+# use modified audioinput plugin for native saving to file:
+cordova plugin add git://github.com/nzilbb/cordova-plugin-audioinput.git
+
 cordova plugin add cordova-plugin-android-permissions
 cordova plugin add cordova-plugin-compat
 cordova plugin add cordova-plugin-device
 
 #cordova plugin add cordova-plugin-file
+# use modified file plugin for avoiding persistent file system use on "browser" platform, for Chrome
 cordova plugin add git://github.com/nzilbb/cordova-plugin-file.git
 
 cordova plugin add cordova-plugin-app-version
